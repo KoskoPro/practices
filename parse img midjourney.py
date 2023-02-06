d77dc139dd5c15b16c4588fae70ade7132090ee1
Kosko <xanaxko@gmail.com>
2023-02-07 00:20:29 +0100

    Parse AI images from midjourney webpage

--- parse img midjourney.py
+++ parse img midjourney.py
@@ -4,34 +4,29 @@ import requests
 from PIL import Image
 import os
 
-#download dynamic webpage
-url='https://www.midjourney.com/showcase/recent/'
+# download dynamic webpage
+url = 'https://www.midjourney.com/showcase/recent/'
 browser = webdriver.Chrome()
 browser.get(url)
-page=browser.page_source
+page = browser.page_source
 
-#parsing links
+# parsing links
 bs_page = BeautifulSoup(page, 'html.parser')
-bs_list_of_links=bs_page.find_all(class_='aspect-auto')
+bs_list_of_links = bs_page.find_all(class_='aspect-auto')
 
-#save files
+# save files
 for element in bs_list_of_links:
-    link=element.get('src')
+    link = element.get('src')
     filename = link.split('/')[-2]
-    path_name = './images/'+filename
+    path_name = './images/' + filename
 
-    #download file
+    # download file
     data = requests.get(link)
-    open(path_name,'wb').write(data.content)
+    open(path_name, 'wb').write(data.content)
 
-    #converting file webp to jpg
+    # converting file webp to jpg
     im = Image.open(path_name).convert('RGB')
-    im.save(path_name+'.jpg','jpeg')
+    im.save(path_name + '.jpg', 'jpeg')
 
-    #delete webp file
+    # delete webp file
     os.remove(path_name)
-
-
-
-
-
